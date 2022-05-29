b141dab31ef98128327f5c7357222716e8c93414
Bertold F <bertoldf@Bertolds-MacBook-Pro.local>
2022-05-28 18:43:05 -0700

    small small

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,24 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
-
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+const VehicleModule = require('./vehicle').Vehicle
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage)  {
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 30;
+        this.scheduleService = none;
+    } 
+}
+let myCar = new Car('Honda', 'accord', '2017', 'navy', 160000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
